bc82f292de47ff4ac64b9c2f9e561617827561c0
70_10 <7010.dev@gmail.com>
2019-03-01 14:35:47 +0900

    [add] gatsby-plugin-twitter

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sass",
+    "gatsby-plugin-twitter",
     {
       resolve: "gatsby-transformer-remark",
       options: {
