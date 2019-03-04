02e10e7e38f612c9d91d046372af76dc56dc8cce
70_10 <7010.dev@gmail.com>
2019-03-04 13:16:29 +0900

    [add] article-footer

--- src/templates/article.js
+++ src/templates/article.js
@@ -29,17 +29,77 @@ export default ({
         __html: body.childMarkdownRemark.html
       }}
     />
+    <Footer />
   </Layout>
 );
 
 const Tags = ({ tags }) => (
-  <div class="tags">
+  <div className="tags">
     {tags.map(tag => (
       <Link to={`/tags/${tag}`} className="tag is-warning">
         {tag}
       </Link>
     ))}
   </div>
+);
+
+const Footer = () => (
+  <footer className="footer">
+    <article className="media">
+      <figure className="media-left">
+        <p className="image is-64x64">
+          <img src="/icon.jpg" alt="Icon" />
+        </p>
+      </figure>
+      <div className="media-content">
+        <div className="content">
+          <p>
+            written by{" "}
+            <strong>
+              <a
+                href="https://twitter.com/70_10"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                70_10
+              </a>
+            </strong>
+          </p>
+        </div>
+        <nav className="breadcrumb" aria-label="breadcrumbs">
+          <ul>
+            <li>
+              <a
+                href="https://twitter.com/70_10"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/70-10"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://qiita.com/70_10"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Qiita
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </article>
+  </footer>
 );
 
 export const query = graphql`
