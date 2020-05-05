39fff526ef657f18f9b6dc0732ca919d9e4f99cc
70_10 <7010.dev@gmail.com>
2020-05-06 08:45:26 +0900

    [fix] accessibility

--- src/layouts/defaultLayout.tsx
+++ src/layouts/defaultLayout.tsx
@@ -27,12 +27,9 @@ type Props = {
 
 const Head: FC<{ title: string }> = ({ title }) => {
   return (
-    <Helmet>
+    <Helmet htmlAttributes={{ lang: "ja" }}>
       <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, maximum-scale=1"
-      />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="format-detection" content="telephone=no" />
       <meta name="description" content="Blog at 70-10.net" />
