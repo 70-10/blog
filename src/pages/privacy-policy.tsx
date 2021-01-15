ab1ff521c15aefe275540cc691cfce087e2de277
70_10 <7010.dev@gmail.com>
2021-01-15 15:18:31 +0900

    Add PrivacyPolicy CSS

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,41 +1,41 @@
 import React, { FC } from "react";
 import Layout from "../layouts/defaultLayout";
+import styles from "./privacy-policy.module.css";
 
 const PrivacyPolicyPage: FC = () => {
   return (
     <Layout>
-      <div className="hero">
-        <div className="hero-body">
-          <div className="columns is-centered">
-            <div className="column is-8">
-              <h1 className="title">プライバシーポリシー</h1>
-              <div className="content">
-                <h2>アクセス解析について</h2>
-                <p>
-                  当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
-                  この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関しての詳細は
-                  <a
-                    href="http://www.google.com/analytics/terms/jp.html"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Googleアナリティクスサービス利用規約のページ
-                  </a>
-                  や
-                  <a
-                    href="http://www.google.co.jp/policies/technologies/ads/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Googleポリシーと規約ページ
-                  </a>
-                  をご覧ください。
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <h1 className={styles.title}>プライバシーポリシー</h1>
+      <hr className={styles.line} />
+      <article className={styles.content}>
+        <h3>アクセス解析について</h3>
+        <p>
+          当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。
+          <br />
+          このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
+          <br />
+          この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
+        </p>
+        <p>
+          この規約に関しての詳細は
+          <a
+            href="http://www.google.com/analytics/terms/jp.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Googleアナリティクスサービス利用規約のページ
+          </a>
+          や
+          <a
+            href="https://policies.google.com/technologies/ads?gl=jp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Googleポリシーと規約ページ
+          </a>
+          をご覧ください。
+        </p>
+      </article>
     </Layout>
   );
 };
