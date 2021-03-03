8a499d845ee549c16247d5020fb23d60bb423c3c
70_10 <7010.dev@gmail.com>
2021-03-03 16:11:43 +0900

    Update Article css

--- src/templates/article.tsx
+++ src/templates/article.tsx
@@ -76,7 +76,7 @@ const Article: FC<Props> = ({ data, location }) => {
         </section>
 
         <div
-          className="prose prose-green max-w-none mt-4"
+          className="prose prose-green max-w-none mt-6"
           dangerouslySetInnerHTML={{
             __html: body?.childMarkdownRemark?.html || "",
           }}
