17d22f3ab4d83c890c04b72479026b31ce22ce72
70_10 <7010.dev@gmail.com>
2023-01-18 10:09:10 +0900

    Modify level component

--- src/components/level.tsx
+++ src/components/level.tsx
@@ -17,27 +17,36 @@ const Level: React.FC<Props> = ({ articles }) => (
       <p className={styles.year}>All</p>
       <p className={styles.article_count}>{articles.length}</p>
     </div>
-    {Array.from({ length: 5 }, (_, i) => {
-      const targetYear = cdate()
-        .add(i * -1, "year")
-        .format("YYYY");
-      return (
-        <div key={i} className={styles.item}>
-          <p className={styles.year}>{targetYear}</p>
-          <p className={styles.article_count}>
-            {
-              articles.filter(({ node }) => {
-                const first = cdate(`${targetYear}-01-01 00:00:00`);
-                const last = cdate(`${targetYear}-12-31 23:59:59`);
-
-                const target = cdate(node.publishDate!);
-                return first <= target && target <= last;
-              }).length
+    {Array.from(
+      {
+        length: new Set(
+          articles.map(({ node }) => {
+            if (!node.publishDate) {
+              return null;
             }
-          </p>
-        </div>
-      );
-    })}
+
+            return cdate(node.publishDate).format("YYYY");
+          })
+        ).size,
+      },
+      (_, i) => {
+        const targetYear = cdate().add(-i, "year").format("YYYY");
+        return (
+          <div key={i} className={styles.item}>
+            <p className={styles.year}>{targetYear}</p>
+            <p className={styles.article_count}>
+              {
+                articles.filter(
+                  ({ node }) =>
+                    node.publishDate &&
+                    cdate(node.publishDate).format("YYYY") === targetYear
+                ).length
+              }
+            </p>
+          </div>
+        );
+      }
+    )}
   </nav>
 );
 
