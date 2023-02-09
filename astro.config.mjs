e5d87539e235e74ab6c6eab4eb9d10e503fc4ce4
70_10 <7010.dev@gmail.com>
2023-02-09 11:53:20 +0900

    Add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 import codeTitle from "remark-code-titles";
 
 export default defineConfig({
   site: "https://blog.70-10.net/",
-  integrations: [tailwind()],
+  integrations: [tailwind(), compress()],
   markdown: {
     remarkPlugins: ["remark-gfm", "remark-smartypants", codeTitle],
     shikiConfig: {
