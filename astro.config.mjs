cb4165ee0908204fe4575272d31ce22694e42660
70_10 <7010.dev@gmail.com>
2023-02-15 00:48:03 +0900

    Add /og/[slug].png

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 import codeTitle from "remark-code-titles";
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://blog.70-10.net/",
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), react()],
   markdown: {
     remarkPlugins: ["remark-gfm", "remark-smartypants", codeTitle],
     shikiConfig: {
