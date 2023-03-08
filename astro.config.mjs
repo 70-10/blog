001df7b32d5c4babf864487e2c1a2bb8968c6706
70_10 <7010.dev@gmail.com>
2023-03-08 10:08:05 +0900

    Add partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import partytown from "@astrojs/partytown";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
@@ -7,7 +8,16 @@ import codeTitle from "remark-code-titles";
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.70-10.net/",
-  integrations: [tailwind(), compress(), react()],
+  integrations: [
+    tailwind(),
+    compress(),
+    react(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   markdown: {
     remarkPlugins: ["remark-gfm", "remark-smartypants", codeTitle],
     shikiConfig: {
